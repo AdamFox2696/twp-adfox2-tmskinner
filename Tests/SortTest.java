e96bb17f90ac575f62bb6ecbd784f09eba85ad77
tms326 <31251865+tms326@users.noreply.github.com>
2017-09-18 15:31:02 -0400

    Sort test works

---
+++ Tests/SortTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SortTest {
+
+    @Test
+    public void SortTest(){
+        Sorter sorter = new Sorter();
+        int[] input = {9,67,23,5,68,120,38,74,26,84,47,78,120,87,32};
+        int[] sortedInput ={5,9,23,26,32,38,47,67,68,74,78,84,87,120,120};
+        int[] output = sorter.InsertionSort(input);
+        Assert.assertArrayEquals(sortedInput, output);
+    }
+}
